cc0902d646a5b3e69c4ea1a30e552f509f394730
Pham Vu Thien Bao <97822939+BaoPhamm@users.noreply.github.com>
2023-05-16 17:56:56 +0700

    fix code review

--- src/main/java/com/bao/Main.java
+++ src/main/java/com/bao/Main.java
@@ -1,5 +1,6 @@
 package com.bao;
 
+import com.bao.exception.NotSupportedFileExtensionException;
 import com.bao.importer.FileImporter;
 import com.bao.importer.ImporterFactory;
 import com.bao.model.Company;
@@ -12,85 +13,95 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
 
 public class Main {
-    private static final String FILE_PATH = "src/main/resources/import/companies.csvv";
-    //    private static final String FILE_PATH = "src/main/resources/import/companies_bigdata.csv";
-    private static final String FOLDER_PATH = "src/main/resources/import";
+
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
+        if (args.length < 2) {
+            logger.info("Not enough args, please provide:\n1: file path\n2: country");
+            return;
+        }
 
+        String filePath = args[0];
+        String country = args[1];
         List<Company> companies;
+        Predicate<Company> predicateCountry = company -> country.equals(company.getCountry());
+        Predicate<Company> predicateIsHeadQuarter = Company::getHeadQuarter;
+        Comparator<Company> comparatorCapitalDesc = (o1, o2) -> o2.getCapital().compareTo(o1.getCapital());
 
         try {
-            companies = importFile(FILE_PATH);
-            printTotalCapitalOfHeadquartersLocatedInCH(companies);
-//            printTotalCapitalOfHeadquartersLocatedInCHBigData(FILE_PATH);
-            printNameOfCompaniesInCHSortedByCapitalDesc(companies);
-            monitorFolderImport(FOLDER_PATH);
-
+            companies = importFile(filePath);
+            printTotalCapitalOfHeadquartersLocatedInCountry(companies, country, predicateCountry.and(predicateIsHeadQuarter));
+            printNameOfCompaniesInCountry(companies, country, predicateCountry, comparatorCapitalDesc);
+            monitorFolderImport(filePath, country, predicateCountry, predicateIsHeadQuarter, comparatorCapitalDesc);
 
         } catch (IOException e) {
-            System.out.println("Failed to import file: " + e.getMessage());
-            e.printStackTrace();
+            logger.severe("Failed to import file: " + e.getMessage());
         } catch (InterruptedException e) {
-            System.out.println("InterruptedException: " + e.getMessage());
-            e.printStackTrace();
+            logger.severe("InterruptedException: " + e.getMessage());
             Thread.currentThread().interrupt();
         }
     }
 
     private static List<Company> importFile(String path) throws IOException {
         Path filePath = Paths.get(path);
-        String extension = getFileExtension(filePath);
+        String extension = getFileExtension(filePath)
+                .orElseThrow(() -> new NotSupportedFileExtensionException("Unsupported file extension."));
 
         FileImporter fileImporter = ImporterFactory.getImporter(extension);
         return fileImporter.importFile(filePath);
     }
 
-    private static String getFileExtension(Path path) {
+    private static Optional<String> getFileExtension(Path path) {
         String fileName = path.getFileName().toString();
         int dotIndex = fileName.lastIndexOf(".");
         if (dotIndex > 0 && dotIndex < fileName.length() - 1) {
-            return fileName.substring(dotIndex + 1);
+            return Optional.of(fileName.substring(dotIndex + 1));
         } else {
-            return "";
+            return Optional.empty();
         }
     }
 
     /* print total capital of headquarters located in “CH” */
-    private static void printTotalCapitalOfHeadquartersLocatedInCH(List<Company> companies) {
+    private static void printTotalCapitalOfHeadquartersLocatedInCountry(List<Company> companies,
+                                                                        String country,
+                                                                        Predicate<Company> predicate) {
         int totalCapital = companies.stream()
-                .filter(company -> company.getCountry().equals("CH")
-                        && company.getHeadQuarter())
+                .filter(predicate)
                 .map(Company::getCapital)
                 .reduce(0, Integer::sum);
-        System.out.println("Total capital of headquarters located in CH: " + totalCapital);
-    }
-
-    private static void printTotalCapitalOfHeadquartersLocatedInCHBigData(String path) throws IOException {
-        Path filePath = Paths.get(path);
-        String extension = getFileExtension(filePath);
-
-        FileImporter fileImporter = ImporterFactory.getImporter(extension);
-        System.out.println("Total capital of headquarters located in CH: " +
-                fileImporter.getTotalCapitalOfHeadquartersLocatedInCH(filePath));
+        System.out.println("Total capital of headquarters located in " + country + ": " + totalCapital);
     }
 
     /* name of companies that the country is in “CH”. The list is sorted descending by capital */
-    private static void printNameOfCompaniesInCHSortedByCapitalDesc(List<Company> companies) {
-        System.out.println("Name of companies that the country is in CH (sorted descending by capital): ");
+    private static void printNameOfCompaniesInCountry(List<Company> companies,
+                                                      String country,
+                                                      Predicate<Company> predicate,
+                                                      Comparator<Company> comparatorCapitalDesc) {
+        System.out.println("Name of companies that the country is in " + country + ":");
         companies.stream()
-                .filter(company -> company.getCountry().equals("CH"))
-                .sorted((o1, o2) -> o2.getCapital().compareTo(o1.getCapital()))
+                .filter(predicate)
+                .sorted(comparatorCapitalDesc)
                 .map(Company::getName)
                 .forEach(System.out::println);
     }
 
-    private static void monitorFolderImport(String folder) throws IOException, InterruptedException {
+    private static void monitorFolderImport(String filePath,
+                                            String country,
+                                            Predicate<Company> predicateCountry,
+                                            Predicate<Company> predicateIsHeadQuarter,
+                                            Comparator<Company> comparatorCapitalDesc) throws IOException, InterruptedException {
         List<Company> companies;
-        Path folderPath = Paths.get(folder);
+        Path path = Paths.get(filePath);
+        Path folderPath = path.getParent();
+        String filename = path.getFileName().toString();
 
         // Create a WatchService and register the folder for events
         WatchService watchService = FileSystems.getDefault().newWatchService();
@@ -102,13 +113,13 @@ public class Main {
             for (WatchEvent<?> event : key.pollEvents()) {
 
                 // Check if the event is for the "companies.csv" file
-                if (event.context().toString().equals("companies_bigdata.csv")) {
-                    System.out.println("companies.csv changed, reimporting....");
+                if (filename.equals(event.context().toString())) {
+                    System.out.println(filename + " changed, reimporting....");
 
                     // Reimport the file
-                    companies = importFile(FILE_PATH);
-                    printTotalCapitalOfHeadquartersLocatedInCH(companies);
-                    printNameOfCompaniesInCHSortedByCapitalDesc(companies);
+                    companies = importFile(filePath);
+                    printTotalCapitalOfHeadquartersLocatedInCountry(companies, country, predicateCountry.and(predicateIsHeadQuarter));
+                    printNameOfCompaniesInCountry(companies, country, predicateCountry, comparatorCapitalDesc);
                 }
             }
             // Reset the key and continue processing events
